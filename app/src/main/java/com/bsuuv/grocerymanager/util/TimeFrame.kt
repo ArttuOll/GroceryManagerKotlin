7fe6aec09b19d7afeb593657d73cd4c90deba5c7
ArttuOll <arttuoll.tech@tutanota.com>
2020-08-21 14:07:57 +0300

    Remove function "value" from TimeFrame
    The function was redundant, since Kotlin convention
    is to access member variables directly instead of
    using getters.

--- app/src/main/java/com/bsuuv/grocerymanager/util/TimeFrame.kt
+++ app/src/main/java/com/bsuuv/grocerymanager/util/TimeFrame.kt
@@ -7,11 +7,6 @@ package com.bsuuv.grocerymanager.util
  * example, if frequency = 3 and time frame = WEEK, then the food item appears on the grocery list 3
  * times a week (given that there are enough grocery days set for that).
  */
-enum class TimeFrame(val mValue: Int) {
+enum class TimeFrame(val value: Int) {
     WEEK(1), TWO_WEEKS(2), MONTH(4), NULL(-2);
-
-    /**
-     * @return Integer representation of the TimeFrame
-     */
-    fun value(): Int = mValue
 }
