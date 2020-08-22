f721724a16e8fb8e58ec0331489db5d5111fa6d3
ArttuOll <arttuoll.tech@tutanota.com>
2020-08-22 14:51:52 +0300

    Convert FoodItemDatabase
    Name changed from FoodItemRoomDatabase, since it contained too much
    information.

---
+++ app/src/main/java/com/bsuuv/grocerymanager/data/db/FoodItemDatabase.kt
@@ -0,0 +1,46 @@
+package com.bsuuv.grocerymanager.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.bsuuv.grocerymanager.data.db.dao.FoodItemDao
+import com.bsuuv.grocerymanager.data.db.entity.FoodItemEntity
+
+/**
+ * A <code>Room</code> database definition for saving food-items.
+ *
+ * @see <a href=https://developer.android.com/topic/libraries/architecture/room>Room Persistence
+ * Library</a>
+ */
+@Database(entities = [FoodItemEntity::class], version = 1)
+abstract class FoodItemDatabase : RoomDatabase() {
+
+    /**
+     * Defines the type of DAO this <code>Room</code> database should provide.
+     *
+     * @return <code>FoodItemDao</code> that grants access to values stored in
+     * this database
+     */
+    abstract val foodItemDao: FoodItemDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FoodItemDatabase? = null
+
+        fun getInstance(context: Context): FoodItemDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        FoodItemDatabase::class.java,
+                        "fooditem_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
