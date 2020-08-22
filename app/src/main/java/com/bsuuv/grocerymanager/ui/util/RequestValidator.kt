fe7d8a0ff75f6e4210152be518a91d546af69a1e
ArttuOll <arttuoll.tech@tutanota.com>
2020-08-22 13:36:22 +0300

    Convert RequestValidator
    Intents are not needed here anymore as in Kotlin it is possible to
    prevent them from being null. This change also made the doc-comments
    of the functions useless, so they were removed.

---
+++ app/src/main/java/com/bsuuv/grocerymanager/ui/util/RequestValidator.kt
@@ -0,0 +1,31 @@
+package com.bsuuv.grocerymanager.ui.util
+
+import android.app.Activity.RESULT_OK
+
+/**
+ * Utility class for interpreting results of different kinds of <code>Activity</code>-related
+ * requests.
+ */
+class RequestValidator {
+    companion object {
+        const val NONE = 0
+        const val FOOD_ITEM_CREATE_REQUEST = 1
+        const val FOOD_ITEM_EDIT_REQUEST = 2
+        const val REQUEST_IMAGE_CAPTURE = 3
+
+        fun foodItemCreationSuccesful(requestCode: Int, resultCode: Int): Boolean {
+            return requestCode == FOOD_ITEM_CREATE_REQUEST &&
+                    resultCode == RESULT_OK
+        }
+
+        fun foodItemEditSuccesful(requestCode: Int, resultCode: Int): Boolean {
+            return requestCode == FOOD_ITEM_EDIT_REQUEST &&
+                    resultCode == RESULT_OK
+        }
+
+        fun imageCaptureSuccesful(requestCode: Int, resultCode: Int): Boolean {
+            return requestCode == REQUEST_IMAGE_CAPTURE &&
+                    resultCode == RESULT_OK
+        }
+    }
+}
