f6665b23c4fcb2030e22d9c729c33918ece0b105
ArttuOll <arttuoll.tech@tutanota.com>
2020-08-24 12:01:47 +0300

    Convert FoodItemViewModel

---
+++ app/src/main/java/com/bsuuv/grocerymanager/data/viewmodel/FoodItemViewModel.kt
@@ -0,0 +1,31 @@
+package com.bsuuv.grocerymanager.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.bsuuv.grocerymanager.data.FoodItemRepository
+import com.bsuuv.grocerymanager.data.db.entity.FoodItemEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+class FoodItemViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mRepository: FoodItemRepository = FoodItemRepository(application)
+
+    fun getFoodItems(): LiveData<MutableList<FoodItemEntity>> =
+        liveData { emitSource(mRepository.getFoodItems()) }
+
+    fun insert(foodItem: FoodItemEntity) = CoroutineScope(IO).launch {
+        mRepository.insert(foodItem)
+    }
+
+    fun delete(foodItem: FoodItemEntity) = CoroutineScope(IO).launch {
+        mRepository.delete(foodItem)
+    }
+
+    fun update(foodItem: FoodItemEntity) = CoroutineScope(IO).launch {
+        mRepository.update(foodItem)
+    }
+}
